7d08886387b977c68e5ba3e8d52046f1ce6e2060
Ravi972 <64277146+Ravi972@users.noreply.github.com>
2026-01-09 23:34:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // This maps the process.env.API_KEY in your code to the variable during build
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
   };
